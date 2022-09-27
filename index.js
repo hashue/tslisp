dc019c434788c40f5a8abe07b542cfa3cb98ebf0
Hasu <hshue83@gmail.com>
2022-09-27 18:19:29 +0900

    refector

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
-function parse(str){
+function parse(str) {
   process.stdout.write(`> ${str}\n`);
   str = str.replaceAll('(', ' ( ').replaceAll(')', ' ) ').split(/\s/);
-
 
   let expr = [];// ex: ['+', 1, 3]
   let stack = []; //ex: [['+' ,1'],['-', 5 4]]
@@ -10,7 +9,6 @@ function parse(str){
 
   while (i < str.length) {
     let token = str[i++];
-
 
     // match atom 
     if (!token.match(/\(|\)/) && token !== '') {
@@ -23,7 +21,7 @@ function parse(str){
       let prev = expr;
       expr = [];
 
-      if (prev?.length !== 0) {
+      if (prev.length !== 0) {
         stack.push(prev)
         prev.push(expr);
       }
@@ -37,6 +35,71 @@ function parse(str){
   }
 }
 
+//
+// Executor
+//
+
+function _eval(stack) {
+  let paren = [];
+
+  const stacking = (target, node, data) => {
+    target.push(node);
+    target.push(data);
+  }
+
+  while (true) {
+
+    if (stack.length == 1 && paren.length == 0) {
+      return process.stdout.write(`=> ${stack[0]}\n`);
+    }
+
+
+    //親が残っていたら中間結果を格納
+    if (stack.length == 1 && paren.length != 0) {
+      paren.push(stack[0]);
+      stack = paren;
+      paren = [];
+    }
+
+
+
+
+    //前から読んでいく
+    let node = stack.shift();
+
+    if (node === '+' || node === '-' || node === '*' || node === '/') {
+      let rhs = stack.shift();
+      let lhs = stack.shift();
+
+      if (rhs instanceof Array) {
+        stacking(paren, node, lhs)
+        stack = rhs;
+      }
+
+      if (lhs instanceof Array) {
+        stacking(paren, node, rhs)
+        stack = lhs;
+      }
+
+      //check nest
+      switch (node) {
+        case '+':
+          stack.push(rhs + lhs);
+          break;
+        case '-':
+          stack.push(rhs - lhs);
+          break;
+        case '*':
+          stack.push(rhs * lhs);
+          break;
+        case '/':
+          stack.push(rhs / lhs);
+          break;
+      }
+    }
+  }
+}
+
 
 //
 // Test
@@ -45,7 +108,7 @@ function parse(str){
 let ast = parse("(+ 1 (- 5 2)))")
 _eval(ast) //4
 
- ast = parse("(* 1 2)")
+ast = parse("(* 1 2)")
 
 _eval(ast) //2
 
@@ -56,115 +119,3 @@ _eval(ast) //5
 ast = parse("(/ 24 (+ 2 4))")
 
 _eval(ast) //4
-
-//
-// Executor
-//
-
-//TODO: impl Eval function
-
-
-function _eval(stack){
-  let paren = [];
-
-  let flag = true;
-  while(flag){
-
-
-    if (stack.length == 1 && paren.length == 0){
-      return process.stdout.write(`=> ${stack[0]}\n`);
-    }
-
-
-    //親が残っていたら中間結果を格納
-    if (stack.length == 1 && paren.length != 0){
-      paren.push(stack[0]);
-      stack = paren;
-      paren = [];
-    }
-
-
-    
-
-    //前から読んでいく
-    let node = stack.shift();
-
-    if(node === "+"){
-      let rhs = stack.shift();
-      let lhs = stack.shift();
-
-
-      if(rhs instanceof Array){
-        stacking(paren,node,lhs)
-        stack = rhs;
-      }
-
-      if(lhs instanceof Array){
-        stacking(paren,node,rhs)
-        stack = lhs;
-      }
-
-
-      //check nest
-
-      stack.push(rhs + lhs);
-    }
-
-    if(node === "-"){
-      let rhs = stack.shift();
-      let lhs = stack.shift();
-
-      if(rhs instanceof Array){
-        stacking(paren,node,lhs)
-        stack = rhs;
-      }
-
-      if(lhs instanceof Array){
-        stacking(paren,node,rhs)
-        stack = lhs;
-      }
-
-
-      stack.push(rhs - lhs);
-    }
-
-    if(node === "*"){
-      let rhs = stack.shift();
-      let lhs = stack.shift();
-
-      if(rhs instanceof Array){
-        stacking(paren,node,lhs)
-        stack = rhs;
-      }
-
-      if(lhs instanceof Array){
-        stacking(paren,node,rhs)
-        stack = lhs;
-      }
-      stack.push(rhs * lhs);
-    }
-    
-    if(node === "/"){
-      let rhs = stack.shift();
-      let lhs = stack.shift();
-
-      if(rhs instanceof Array){
-        stacking(paren,node,lhs)
-        stack = rhs;
-      }
-
-      if(lhs instanceof Array){
-        stacking(paren,node,rhs)
-        stack = lhs;
-      }
-      stack.push(rhs / lhs);
-    }
-  }
-}
-
-function stacking(target,node,data){
-  target.push(node);
-  target.push(data);
-}
-
-
--- util.ts
+++
@@ -1,9 +0,0 @@
-export function assertEq(expect:any,val:any):boolean{
-  expect = String(expect);
-  val = String(val);
-  return (expect === val)? true : false;
-}
-
-export const isArray = <T>(x: T | readonly T[]): x is T[] =>{
-  return Array.isArray(x)
-}
